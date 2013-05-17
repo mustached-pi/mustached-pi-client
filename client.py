304b8172bfcbf3e753c9683756b20979d21f2954
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2013-05-17 12:02:31 +0000

    Now it works and debugs yeah

--- client.py
+++ client.py
@@ -22,6 +22,9 @@ import sys
 # Setting up GPIO to BCM notation
 GPIO.setmode(GPIO.BCM)
 
+# Do not be annoying if I change the mode of a channel
+GPIO.setwarnings(False)
+
 # Defining MUX's GPIO (Multiplexer Input/Output port)
 MUXIO = 22
 
@@ -41,15 +44,18 @@ def aleOn():
 
 # Read value from the port already set with setPort
 def read():
-	GPIO.setup(MUXIO, GPIO.IN)
-	return GPIO.input(MUXIO)
+    GPIO.setup(MUXIO, GPIO.IN)
+    inval = GPIO.input(MUXIO)
+    print "[INPUT] Value: %d" % inval
+    return inval;
 
 # Creates an pulse signal to MUX's out, to change flip flop state
 def pulse():
-	GPIO.setup(MUXIO, GPIO.OUT)
-	GPIO.ouput(MUXIO, GPIO.HIGH)
-	time.sleep(0.01)
-	GPIO.output(MUXIO, GPIO.LOW)
+    print "[PULSE] Pulse sent"
+    GPIO.setup(MUXIO, GPIO.OUT)
+    GPIO.output(MUXIO, GPIO.HIGH)
+    time.sleep(0.01)
+    GPIO.output(MUXIO, GPIO.LOW)
     
 # Function to generate a random MID
 def generateMID():
@@ -58,29 +64,43 @@ def generateMID():
 # Converts port's address from dec to bin and set the GPIO to the
 # right logical level, then activates ALE
 def setPort(port):
-	portb=bin(port)
+    print "[SET] Port %s" % port
+    port = int(port)        # We need an integer for binary conversion
+    portb = bin(port)       # Binary conversion...
+    portb = portb.zfill(4)  # Makes a 4 digits binary number
+                            # (Fills with zeros if needed)
+                            
 	# D -> MSB
-	if portb[2]==1:
-		GPIO.output(21,GPIO.HIGH)
-	else:
-		GPIO.output(21,GPIO.LOW)
-	# C
-	if portb[3]==1:
-		GPIO.output(17,GPIO.HIGH)
-	else:
-		GPIO.output(17,GPIO.LOW)
-	# B
-	if portb[4]==1:
-		GPIO.output(4,GPIO.HIGH)
-	else:
-		GPIO.output(4,GPIO.LOW)
-	# A -> LSB
-	if portb[5]==1:
-		GPIO.output(1,GPIO.HIGH)
-	else:
-		GPIO.output(1,GPIO.LOW)
-	# Activating ALE...
-	aleOn()
+    if portb[0] == "1":
+    	GPIO.output(21,GPIO.HIGH)
+    else:
+    	GPIO.output(21,GPIO.LOW)
+    # C
+    if portb[1] == "1":
+    	GPIO.output(17,GPIO.HIGH)
+    else:
+        GPIO.output(17,GPIO.LOW)
+    # B
+    if portb[2] == "1":
+        GPIO.output(4,GPIO.HIGH)
+    else:
+        GPIO.output(4,GPIO.LOW)
+    # A -> LSB
+    if portb[3] == "1":
+        GPIO.output(1,GPIO.HIGH)
+    else:
+        GPIO.output(1,GPIO.LOW)
+    # Activating ALE...
+    aleOn()
+
+
+# Before doing anything, set ADDRESS PORTS as outputs
+GPIO.setup(1,  GPIO.OUT) # A -> LSB
+GPIO.setup(4,  GPIO.OUT) # B
+GPIO.setup(17, GPIO.OUT) # C
+GPIO.setup(21, GPIO.OUT) # D -> MSB
+# Now remember setting the ADDRESS LATCH ENABLE as output
+GPIO.setup(ALE, GPIO.OUT)
 
 
 
@@ -134,34 +154,45 @@ while 1:
 #Loop -> changes the switch value if different from the previous configuration
 #     -> saves values from sensors
 
-	#creates new array
-	arrayread={}
-	print arrayconf['ports']
-	#Checks the array from the server
+	# Creates new dictionary to collect sensor data
+	arrayread = {}
+    
+	# For each configured port from the server
 	for (port, value) in arrayconf['ports'].iteritems():
-		#Setting up MUX port
+        # Where:
+        #  port     is the port number
+        #  value -> {dictionary}
+        #   value.type  is the port type ("input"/"output")
+        #   value.value is the port value (0 or 1, only output case)
+        
+		# Setting up MUX port
 		setPort(port)
-		#Case output
+        
+		# Case output port
 		if value["type"]=="output":
 			if value["value"]!=arrayprec.get(port, 0):
 				pulse()
-				print "pulse"
 				arrayprec[port]=value["value"]
-		#Case input
+                
+		# Case input/sensor port
 		if value["type"]=="input":
 			arrayread[port]=read()
 			temp = random.randint(0,255)
 			arrayread[port]=temp
 
-	#Converts the array in JSON
-	arrayread={'sid': MID, 'ports': arrayread}
-	arrayread=json.dumps(arrayread)
-	#send array
-	f=urllib2.urlopen( BASEURL + '/endpoint.php',arrayread)
-	#read HTTP response
-	arrayconf=f.read()
-	print arrayconf
-	#Converts JSON json from server in array
+	# Converts the array into JSON
+	arrayread = {'sid': MID, 'ports': arrayread}
+	arrayread = json.dumps(arrayread)
+    
+	# Sends everything to the server
+	f = urllib2.urlopen( BASEURL + '/endpoint.php',arrayread)
+    
+	# Reads the HTTP response for the next loop
+	arrayconf = f.read()
+	# print arrayconf # DEBUG
+    
+	# Decode JSON response into a dictionary
 	arrayconf=json.loads(arrayconf)
-	#One HTTP request every 5 seconds
+    
+	# Now wait before looping again
 	time.sleep(WAIT)
